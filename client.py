c3a79a52dec1c6557b48562aaa667c865ee7adb3
Tim Antkowiak <antkowiak.tim@gmail.com>
2020-03-11 17:15:12 +0100

    tmp

--- client.py
+++ client.py
@@ -2,13 +2,15 @@ import socket
 import time
 import re
 import select
+import sys
 
 HOST = 'me.antkowiak.ddnss.de'
 PORT = 2280
+server = (HOST, PORT)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect((HOST, PORT))
+s.connect(server)
 
 data = s.recv(1024).decode()
 print('Server:', repr(data))
@@ -24,7 +26,6 @@ s.close()
 
 # extract data from server response
 peer_IP = re.findall(r"\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}", data)[0]
-peer_IP = HOST
 print('Peer IP is', peer_IP)
 
 service_port = 51820
@@ -40,17 +41,21 @@ if is_first:
     for udp_socket in udp_socket_list:
         udp_socket.sendto(b'Hello', (peer_IP, service_port))
 
-    print('Punched ' + punch_count + ' holes')
+    print('Punched', punch_count, 'holes')
     print('Wait for peer to find a hole')
 
-    ready_socks,_,_ = select.select(udp_socket_list, [], []) 
-    for udp_socket in ready_socks:
-        data, addr = udp_socket.recvfrom(1024)
-        if(addr):
-            print('Got Holepunch! Sending a hello back')
-            udp_socket.sendto(b'hello!', addr)
+    while True:
+        ready_socks,_,_ = select.select(udp_socket_list, [], [], 50) 
+        for udp_socket in ready_socks:
+            data, addr = udp_socket.recvfrom(1024)
+            if(addr):
+                print('Got Holepunch! Sending a hello back') 
+                udp_socket.sendto(b'hello!', addr)
 
-        udp_socket.close()
+            udp_socket.close()
+        # Repunch after 50 seconds
+        for udp_socket in udp_socket_list:
+            udp_socket.sendto(b'Hello', (peer_IP, service_port))
 
 else:
     try_count = 30
@@ -58,14 +63,16 @@ else:
 
     for i in range(try_count):
         udp_socket_list.append(socket.socket(socket.AF_INET, socket.SOCK_DGRAM))
-        udp_socket_list[i].settimeout(1)
 
-    start_port = 32000
+    port = 32000
     end_port = 65535
     loop_number = 0
     while True:
+        if port == end_port:
+            break
+
         port = start_port
-        print('Try port ' + port + '-' + (port + try_count))
+        print('Try port', port, '-', (port + try_count))
 
         for udp_socket in udp_socket_list:
             udp_socket.sendto(b'Hello', (peer_IP, port))
@@ -73,12 +80,72 @@ else:
 
         ready_socks,_,_ = select.select(udp_socket_list, [], []) 
         for udp_socket in ready_socks:
+            udp_socket.settimeout(1)
             data, addr = udp_socket.recvfrom(1024)
             if(addr):
                 print('Got Holepunch!!!')
                 print(addr)
-        start_port = start_port + try_count + 1
 
     for udp_socket in udp_socket_list:
         udp_socket.close()
+
+# Connect to server 
+server_response[0] = get_response()
+server_response[1] = get_response()
+
+peer_IP = ...
+
+if isFirst(server_response[0]):
+    print('Wait for peer to determine NAT type')
+    data = s.recv(1024)
+
+    if data.decode() == 'UDP port consistent':
+        print('UDP port is consistent!')
+        print('Give peer a second to punch a hole')
+        time.sleep(1)
+
+        print('Connect to peer')
+        udp_socket.sendto((peer_IP, 55555))
+
+        sys.exit()
+
+    punch_count = 25
+    udp_socket_list = []
+
+    print('Punch a few holes...')
+    for punch in range(punch_count):
+        udp_socket_list.append(socket.socket(socket.AF_INET, socket.SOCK_DGRAM))
         
+    for udp_socket in udp_socket_list:
+        udp_socket.sendto(b'Hello', (peer_IP, service_port))
+
+    print('Wait for peer to find a hole...')
+    while True:
+        ready_socks,_,_ = select.select(udp_socket_list, [], [], 50) 
+        for udp_socket in ready_socks:
+            data, addr = udp_socket.recvfrom(1024)
+            if(addr):
+                print('Got Holepunch! Sending a stop signal to server') 
+                s.send(b'Stop!')
+
+        # Repunch after 50 seconds
+        for udp_socket in udp_socket_list:
+            udp_socket.sendto(b'Hello', (peer_IP, service_port))
+
+else:
+    port = 55555
+    if is_udp_port_consistent(port):
+        print('Internal UDP port is equal to external.')
+        s.send(b'UDP port consistent', server)
+
+        print('Punch hole for pre defined port')
+        udp_socket.sendto(b'punch', (peer_IP, port))
+
+        print('Listen on pre defined port', port)
+        data, addr = udp_socket.recvfrom(1024)
+
+        print('Got connection from', addr)
+        sys.exit()
+    
+    s.send(b'UDP port NOT consistent')
+    print('NAT Router is not port consistent. Try the aggressive way...')
--- server.py
+++ server.py
@@ -15,16 +15,27 @@ while True:
     connection1, addr1 = s.accept()      
     print('Got 1st connection from', addr1) 
 
-    connection1.sendall(b'You are the 1st (' + json.dumps(addr1).encode() + '). Wait for peer...') 
+    connection1.sendall(b'You are the 1st (' + json.dumps(addr1).encode() + b'). Wait for peer...') 
 
     connection2, addr2 = s.accept() 
     print('Got 2nd connection from', addr2)
 
-    connection2.sendall(b'You are the 2nd (' + json.dumps(addr2).encode() + ')') 
+    connection2.sendall(b'You are the 2nd (' + json.dumps(addr2).encode() + b')') 
 
     # Inform cients about each other
     connection1.sendall(b'2nd peer: ' + json.dumps(addr2).encode())
     connection2.sendall(b'1st peer: ' + json.dumps(addr1).encode())
+
+    print('Wait for client 2 to determine NAT type')
+    data = connection2.recv(1024)
+    print('Forward answer to client 1:', data.decode())
+    connection1.sendall(data)
+    
+    if data.decode() == 'UDP port NOT consistent':
+        print('Wait for stop signal from client 1')
+        data = connection1.recv(1024)
+        print('Got stop signal. Forward to client 2')
+        connection2.sendall(b'Stop!')
 
     connection1.close()
     connection2.close()
